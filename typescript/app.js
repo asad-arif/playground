b7c37d48aa787a4a91161971a8464d037dfe3ba7
mdotasad <asadofficial01@gmail.com>
2024-07-23 14:22:07 +0500

    typecsript-1

---
+++ typescript/app.js
@@ -0,0 +1,28 @@
+// const sum = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+// let person: personData = {
+//   name: "asad",
+//   age: 54,
+//   single: "true",
+//   city: "islamabad",
+//   roll: "new",
+// };
+// const fruits: (string | number | boolean)[] = [
+//   "apple",
+//   "orange",
+//   "banana",
+//   123,
+//   32,
+//   true,
+// ];
+// const players: [string, string, boolean, string?] = ["asad", "zain", true];
+// enum abc {
+//   apple = 5,
+//   orange = 10,
+//   banana = 15,
+// }
+// console.log(abc);
+var a;
+a = "asad";
+console.warn(a);
---
+++ typescript/app.ts
@@ -0,0 +1,70 @@
+// const sum = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+
+// const result = sum(87, 130);
+
+// console.warn(result);
+
+// const dummy = (str: string) => {
+//   return str;
+// };
+// const result2 = dummy("Hello typescript!");
+// console.warn(result2);
+
+// let a = 10;
+// a = "44";
+
+// let school = true;
+// school = true;
+
+// let a: 5 | 10;
+
+// a = 10;
+
+// console.log(a);
+
+type personData = {
+  name: string;
+  age: number;
+  single: string;
+  city: string;
+  roll?: string;
+};
+
+// let person: personData = {
+//   name: "asad",
+//   age: 54,
+//   single: "true",
+//   city: "islamabad",
+//   roll: "new",
+// };
+
+// const fruits: (string | number | boolean)[] = [
+//   "apple",
+//   "orange",
+//   "banana",
+//   123,
+//   32,
+//   true,
+// ];
+
+// const players: [string, string, boolean, string?] = ["asad", "zain", true];
+
+// enum abc {
+//   apple = 5,
+//   orange = 10,
+//   banana = 15,
+// }
+
+// console.log(abc);
+
+// let a: "asad" | 656;
+
+// a = "asad";
+
+// console.warn(a);
+
+function glow(): undefined {
+  return undefined;
+}
