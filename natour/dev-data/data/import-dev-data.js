b4c53a2e859c14b42716d7346270f4a4384e563f
Asad <asadofficial01@gmail.com>
2024-10-18 21:14:07 +0500

    node app init

---
+++ natour/dev-data/data/import-dev-data.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
+
+dotenv.config({ path: path.resolve(__dirname, './../../config.env') });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
+//READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `tours.json`), 'utf-8')
+);
+const users = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `users.json`), 'utf-8')
+);
+const reviews = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `reviews.json`), 'utf-8')
+);
+
+//WRITE DATA
+const addData = async () => {
+  try {
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('User Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//DELETE DATA
+const deleteData = async () => {
+  try {
+    await User.deleteMany();
+    await Tour.deleteMany();
+    await Review.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') addData();
+else if (process.argv[2] === '--delete') deleteData();
+else console.log(`Can't find what are you looking for`);
